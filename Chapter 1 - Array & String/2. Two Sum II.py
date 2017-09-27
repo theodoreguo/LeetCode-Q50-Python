204440ccbfaab38a43692f3b4065ee7d035ef03d
Zitong Guo <jsguozitong@gmail.com>
2017-09-27 18:25:17 +0800

    Add 2nd solution to 2. Two Sum II (To Test)

--- Chapter 1 - Array & String/2. Two Sum II.py
+++ Chapter 1 - Array & String/2. Two Sum II.py
@@ -1,12 +1,32 @@
 
 """
-O(n) runtime, O(n) space
-
 Question:
 Similar to Question [1. Two Sum], except that the input array is already sorted in ascending order.
 """
 
 class Solution(object):
+    """
+    O(n log n) runtime, O(1) space – Binary search
+    """
+    def __binary_search(a, key, start):
+        l = start, r = len(a) - 1
+        while l <= r:
+            m = (l + r) / 2
+            if a[m] < key:
+                l = m + 1
+            elif a[m] > key:
+                r = m - 1
+            else:
+                return m
+        return -1
+    
+    def two_sum(self, nums, target):
+        for i in range(len(nums) - 1):
+            j = self.__binary_search(nums, target - nums[i], i + 1)
+            if j != -1:
+                return  [i, j]
+        return []
+    
     """
     O(n) runtime, O(1) space – Tow pointers
     Let’s assume we have two indices pointing to the ith and jth elements, Ai and Aj respectively. The sum of Ai and Aj could only fall into one of these three possibilities:
@@ -14,7 +34,7 @@ class Solution(object):
     ii. Ai + Aj < target. Decreasing j isn’t going to help us, as it makes the sum even smaller. Therefore we should increment i.
     iii. Ai + Aj == target. We have found the answer.
     """
-    def twoSum(self, nums, target):
+    def two_sum2(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
@@ -31,8 +51,8 @@ class Solution(object):
                 j -= 1
             else:
                 return [i, j]
-
         return []
 
 if __name__ == '__main__':
-    print Solution().twoSum([3, 5, 7, 12, 23], 28)
+    print Solution().two_sum([3, 5, 7, 12, 23], 28)
+    print Solution().two_sum2([3, 5, 7, 12, 23], 28)
