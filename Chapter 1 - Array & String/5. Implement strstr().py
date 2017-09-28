6c89cf0a9da77a3056b788302dc955dd20467cf0
Zitong Guo <jsguozitong@gmail.com>
2017-09-28 11:50:53 +0800

    Remove useless leading space

--- Chapter 1 - Array & String/5. Implement strstr().py
+++ Chapter 1 - Array & String/5. Implement strstr().py
@@ -1,8 +1,8 @@
 # coding=utf-8
 """
- Question:
- Implement strStr().
- Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+Question:
+Implement strStr().
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 """ 
 
 """
