a4c2ab4650aaee79dbc4e21f704278d926459715
Zitong Guo <jsguozitong@gmail.com>
2017-09-28 11:52:19 +0800

    Optimise solution description

--- Chapter 1 - Array & String/5. Implement strstr().py
+++ Chapter 1 - Array & String/5. Implement strstr().py
@@ -8,14 +8,14 @@ Returns the index of the first occurrence of needle in haystack, or -1 if needle
 """
  O(nm) runtime, O(1) space – Brute force
  
-i. needle or haystack is empty. If needle is empty, always return 0. If haystack is empty, then there will
-   always be no match (return –1) unless needle is also empty which 0 is returned.
-ii. needle’s length is greater than haystack’s length. Should always return –1. 
+i.   needle or haystack is empty. If needle is empty, always return 0. If haystack is empty, then there will
+     always be no match (return –1) unless needle is also empty which 0 is returned.
+ii.  needle’s length is greater than haystack’s length. Should always return –1. 
 iii. needle is located at the end of haystack. For example, “aaaba” and “ba”. Catch possible off-by-one errors.
-iv. needle occur multiple times in haystack. For example, “mississippi” and “issi”. It should return index 2
-    as the first match of “issi”.
-v. Imagine two very long strings of equal lengths = n, haystack = “aaa...aa” and needle = “aaa...ab”. You should
-   not do more than n character comparisons, or else your code will get Time Limit Exceeded in OJ.
+iv.  needle occur multiple times in haystack. For example, “mississippi” and “issi”. It should return index 2
+     as the first match of “issi”.
+v.   Imagine two very long strings of equal lengths = n, haystack = “aaa...aa” and needle = “aaa...ab”. You should
+     not do more than n character comparisons, or else your code will get Time Limit Exceeded in OJ.
 """
 class Solution(object):
     def strStr(self, haystack, needle):
