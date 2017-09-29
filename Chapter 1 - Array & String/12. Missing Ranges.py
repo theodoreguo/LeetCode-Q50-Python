2f2e05f485ad8a52d328ead5685c2cf5ccf26e1f
Zitong Guo <jsguozitong@gmail.com>
2017-09-29 16:17:21 +0800

    Create 12. Missing Ranges.py

---
+++ Chapter 1 - Array & String/12. Missing Ranges.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+"""
+Question:
+Given a sorted integer array where the range of elements are [0, 99] inclusive, return its missing ranges.
+For example, given [0, 1, 3, 50, 75], return [“2”, “4->49”, “51->74”, “76->99”]
+
+Example Questions Candidate Might Ask:
+Q: What if the given array is empty?
+A: Then you should return [“0->99”] as those ranges are missing.
+Q: What if the given array contains all elements from the ranges?
+A: Return an empty list, which means no range is missing.
+"""
+
+# O(n) runtime, O(1) space
+class Solution(object):
+    def missing_ranges(self, nums, lower, upper):
+        """
+        :type nums: List[int]
+        :type lower: int
+        :type upper: int
+        :rtype: List[str]
+        """
+
+        def getRange(lower, upper):
+            if lower == upper:
+                return "{}".format(lower)
+            else:
+                return "{}->{}".format(lower, upper)
+
+        ranges = []
+        pre = lower - 1
+
+        for i in xrange(len(nums) + 1):
+            if i == len(nums):
+                cur = upper + 1
+            else:
+                cur = nums[i]
+            if cur - pre >= 2:
+                ranges.append(getRange(pre + 1, cur - 1))
+
+            pre = cur
+
+        return ranges
+
+if __name__ == '__main__':
+    print Solution().missing_ranges([0, 1, 3, 50, 75], 0, 99)
