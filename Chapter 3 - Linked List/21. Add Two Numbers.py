1218f66d244da33066e64ee9939fbad8a6d81c9a
Zitong Guo <jsguozitong@gmail.com>
2017-10-12 14:41:53 +0800

    Update 21. Add Two Numbers.py

--- Chapter 3 - Linked List/21. Add Two Numbers.py
+++ Chapter 3 - Linked List/21. Add Two Numbers.py
@@ -8,7 +8,7 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 """
 
-# Definition for singly-linked list.
+# Definition for singly-linked list
 class ListNode:
     def __init__(self, x):
         self.val = x
