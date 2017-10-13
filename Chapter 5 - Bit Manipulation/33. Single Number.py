adca9e6eefbe1fadcd1e68bc0fc16022f90218b1
Zitong Guo <jsguozitong@gmail.com>
2017-10-13 11:29:04 +0800

    Update 33. Single Number.py

--- Chapter 5 - Bit Manipulation/33. Single Number.py
+++ Chapter 5 - Bit Manipulation/33. Single Number.py
@@ -1,56 +1,36 @@
 # coding=utf-8
 """
 Question:
-Given a linked list, swap every two adjacent nodes and return its head.
-For example,
-Given 1 -> 2 -> 3 -> 4, you should return the list as 2 -> 1 -> 4 -> 3.
-Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
 Example Questions Candidate Might Ask:
-Q: What if the number of nodes in the linked list has only odd number of nodes?
-A: The last node should not be swapped.
+Q: Does the array contain both positive and negative integers?
+A: Yes.
+Q: Could any element appear more than twice?
+A: No.
 """
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-    def __repr__(self):
-        if self:
-            return "{} -> {}".format(self.val, self.next)
 
 """
 O(n) runtime, O(1) space
 
-Let’s assume p, q, r are the current, next, and next’s next node.
-We could swap the nodes pairwise by adjusting where it’s pointing next: 
-q.next = p
-p.next = r
-The above operations transform the list from {p -> q -> r -> s} to {q -> p -> r -> s}. 
-If the next pair of nodes exists, we should remember to connect p’s next to s. 
-Therefore, we should record the current node before advancing to the next pair of nodes.
- 
-To determine the new list’s head, you look at if the list contains two or more elements. Basically, these extra conditional 
-statements could be avoided by inserting an extra node (also known as the dummy head) to the front of the list.
+XOR-ing a number with itself is zero. If we XOR all numbers together, it would effectively cancel out all elements that 
+appear twice leaving us with only the single number. As the XOR operation is both commutative and associative, the order 
+in how you XOR them does not matter.
 """
-class Solution(object):
-    def swap_pairs(self, head):
-        dummy = ListNode(0)
-        dummy.next = head
-        p = head
-        prev = dummy
-        while p and p.next:
-            q, r = p.next, p.next.next
-            prev.next = q
-            q.next = p
-            p.next = r
-            prev = p
-            p = r
-        return dummy.next
+class Solution:
+    def single_number(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num = 0
+        for x in nums:
+            num ^= x
+        return num
 
 if __name__ == '__main__':
-    head = ListNode(1)
-    head.next, head.next.next, head.next.next.next = ListNode(2), ListNode(3), ListNode(4)
-    print Solution().swap_pairs(head)
+    print Solution().single_number([2, 3, 6, 4, 4, 5, 3, 6, 2])
+    print Solution().single_number([0, 6, 0])
