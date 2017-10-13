0c135c2fa899c87cf5afd2a0f8d8e12468e7d8d5
Zitong Guo <jsguozitong@gmail.com>
2017-10-13 17:58:12 +0800

    Update 42. Climbing Stairs.py

--- Chapter 8 - Dynamic Programming/42. Climbing Stairs.py
+++ Chapter 8 - Dynamic Programming/42. Climbing Stairs.py
@@ -1,56 +1,68 @@
 # coding=utf-8
 """
 Question:
-Given a linked list, swap every two adjacent nodes and return its head.
-For example,
-Given 1 -> 2 -> 3 -> 4, you should return the list as 2 -> 1 -> 4 -> 3.
-Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+You are climbing a staircase. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
-Example Questions Candidate Might Ask:
-Q: What if the number of nodes in the linked list has only odd number of nodes?
-A: The last node should not be swapped.
+Note: Given n will be a positive integer.
 """
 
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
 
-    def __repr__(self):
-        if self:
-            return "{} -> {}".format(self.val, self.next)
 
 """
-O(n) runtime, O(1) space
+O(n) runtime, O(1) space – Dynamic programming
 
-Let’s assume p, q, r are the current, next, and next’s next node.
-We could swap the nodes pairwise by adjusting where it’s pointing next: 
-q.next = p
-p.next = r
-The above operations transform the list from {p -> q -> r -> s} to {q -> p -> r -> s}. 
-If the next pair of nodes exists, we should remember to connect p’s next to s. 
-Therefore, we should record the current node before advancing to the next pair of nodes.
- 
-To determine the new list’s head, you look at if the list contains two or more elements. Basically, these extra conditional 
-statements could be avoided by inserting an extra node (also known as the dummy head) to the front of the list.
+Set base cases f(1) = 1, f(2) = 2.
+We can calculate f(n) easily by storing previous values in an one dimension array and work our way up to n. Heck, we can 
+even optimize this further by storing just the previous two values.
 """
-class Solution(object):
-    def swap_pairs(self, head):
-        dummy = ListNode(0)
-        dummy.next = head
-        p = head
-        prev = dummy
-        while p and p.next:
-            q, r = p.next, p.next.next
-            prev.next = q
-            q.next = p
-            p.next = r
-            prev = p
-            p = r
-        return dummy.next
+class Solution:
+    def climb_stairs(self, n):
+        """
+        :type n: int
+        :rtype int
+        """
+        if n == 1:
+            return 1
+        p, q = 1, 1
+        for _ in range(2, n + 1):
+            temp = q
+            q += p
+            p = temp
+        return q
+
+"""
+O(2^n) runtime, O(n) space
+
+Define:
+f(n) = number of ways climbing to the nth step. 
+To reach to the nth step, only two choices available:
+1. Advance one step from the n – 1th step.
+2. Advance two steps from the n – 2th step.
+Therefore, f(n) = f(n – 1) + f(n – 2), which is the exact same recurrence formula defined by the Fibonacci sequence 
+(with different base cases, though).
+Set base cases f(1) = 1, f(2) = 2.
+"""
+class Solution2:
+    def climb_stairs(self, n):
+        steps = [0] * (n + 1)
+        return self.calc_ways(n, steps)
+
+    def calc_ways(self, n, steps):
+        if n < 0:
+            return 0
+        if n == 0:
+            return 1
+        if steps[n] != 0:
+            return steps[n]
+        steps[n] = self.calc_ways(n - 1, steps) + self.calc_ways(n - 2, steps)
+        return steps[n]
 
 if __name__ == '__main__':
-    head = ListNode(1)
-    head.next, head.next.next, head.next.next.next = ListNode(2), ListNode(3), ListNode(4)
-    print Solution().swap_pairs(head)
+    print Solution().climb_stairs(1)
+    print Solution().climb_stairs(2)
+    print Solution().climb_stairs(6)
+    print '-----' * 5
+    print Solution2().climb_stairs(1)
+    print Solution2().climb_stairs(2)
+    print Solution2().climb_stairs(6)
