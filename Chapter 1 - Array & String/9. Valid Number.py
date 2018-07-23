8eea9ce4a5c02df13e7e99ee5d5891ece46e7cf1
Zitong Guo <jsguozitong@gmail.com>
2018-07-23 17:48:29 +0800

    Add multiple solutions for 9. Valid Number

--- Chapter 1 - Array & String/9. Valid Number.py
+++ Chapter 1 - Array & String/9. Valid Number.py
@@ -76,13 +76,134 @@ class Solution2(object):
         import re
         return bool(re.match("^\s*[\+-]?((\d+(\.\d*)?)|\.\d+)([eE][\+-]?\d+)?\s*$", s))
 
+class Solution3(object):
+    def remove_spaces(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        n = len(s)
+        i = 0
+        j = n - 1
+        while i < n and s[i] == ' ':
+            i += 1
+        while j > -1 and s[j] == ' ':
+            j -= 1
+        return s[i:j+1]
+
+    def valid_number(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = self.remove_spaces(s)
+        n = len(s)
+
+        if n == 0:
+            return False
+
+        i = 0
+
+        hasDigit = False
+        dotFlag = False
+        eFlag = False
+        hasSign = False
+
+        while i < n:
+            if s[i].isdigit():
+                i += 1
+                hasDigit = True
+                hasSign = True
+            elif not dotFlag and s[i] == '.':
+                i += 1
+                dotFlag = True
+                hasSign = True
+            elif hasDigit and not eFlag and (s[i] == 'e' or s[i] == 'E'):
+                i += 1
+                dotFlag = True
+                eFlag = True
+                hasDigit = False
+                hasSign = False
+            elif not hasDigit and not hasSign and (s[i] == '+' or s[i] == '-'):
+                i += 1
+                hasSign = True
+            else:
+                return False
+
+        if hasDigit:
+            return True
+        else:
+            return False
+
+# Reference: https://leetcode.com/problems/valid-number/discuss/23728/A-simple-solution-in-Python-based-on-DFA
+class Solution4(object):
+    def valid_number(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # define a DFA
+        state = [{},
+                 {'blank': 1, 'sign': 2, 'digit': 3, '.': 4},
+                 {'digit': 3, '.': 4},
+                 {'digit': 3, '.': 5, 'e': 6, 'blank': 9},
+                 {'digit': 5},
+                 {'digit': 5, 'e': 6, 'blank': 9},
+                 {'sign': 7, 'digit': 8},
+                 {'digit': 8},
+                 {'digit': 8, 'blank': 9},
+                 {'blank': 9}]
+        currentState = 1
+        for c in s:
+            if c >= '0' and c <= '9':
+                c = 'digit'
+            if c == ' ':
+                c = 'blank'
+            if c in ['+', '-']:
+                c = 'sign'
+            if c not in state[currentState].keys():
+                return False
+            currentState = state[currentState][c]
+        if currentState not in [3, 5, 8, 9]:
+            return False
+        return True
+
+# Reference: https://leetcode.com/problems/valid-number/discuss/23739/Easy-Python-Solution-68-ms-beats-100
+class Solution5(object):
+    def valid_number(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        try:
+            float(s)
+        except ValueError:
+            return False
+        else:
+            return True
+
 if __name__ == '__main__':
     print Solution().valid_number(" 0.1 ")
     print Solution().valid_number("abc")
     print Solution().valid_number("1 a")
     print Solution().valid_number("2e10")
-
+    print "*" * 10
     print Solution2().valid_number(" 0.1 ")
     print Solution2().valid_number("abc")
     print Solution2().valid_number("1 a")
     print Solution2().valid_number("2e10")
+    print "*" * 10
+    print Solution3().valid_number(" 0.1 ")
+    print Solution3().valid_number("abc")
+    print Solution3().valid_number("1 a")
+    print Solution3().valid_number("2e10")
+    print "*" * 10
+    print Solution4().valid_number(" 0.1 ")
+    print Solution4().valid_number("abc")
+    print Solution4().valid_number("1 a")
+    print Solution4().valid_number("2e10")
+    print "*" * 10
+    print Solution5().valid_number(" 0.1 ")
+    print Solution5().valid_number("abc")
+    print Solution5().valid_number("1 a")
+    print Solution5().valid_number("2e10")
